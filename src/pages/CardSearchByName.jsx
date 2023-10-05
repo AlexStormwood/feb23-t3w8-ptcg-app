83b7604eb8ea9ab5d70f718bf37de8f39a7d4781
Alex H <alex.holder@bigfootds.com>
2023-10-05 20:47:40 +1100

    progress on the API request

--- src/pages/CardSearchByName.jsx
+++ src/pages/CardSearchByName.jsx
@@ -1,6 +1,37 @@
+import { useContext, useEffect, useState } from "react"
+import { ApiContext } from "../contexts/ApiContext";
+import { useParams } from "react-router-dom";
 
 
 export default function CardSearchByName() {
+
+	// search results 
+	const [searchResults, setSearchResults] = useState();
+
+	// api URL 
+	const {api} = useContext(ApiContext);
+
+	// route param for the pokemon name 
+	const {pokemonName} = useParams();
+
+	// api key 
+	let apiKey = "";
+
+	useEffect(() => {
+		console.log("Card search component has mounted! Making a fetch request now...");
+
+		async function apiRequest(){
+			let queryParams = new URLSearchParams({
+				q: 'name:' + pokemonName
+			})
+			let response = await fetch(api + 'cards?' + queryParams, {
+				headers: {
+					'X-Api-Key': apiKey
+				}
+			});
+		}
+
+	}, []);
 
 	return (
 		<div>
