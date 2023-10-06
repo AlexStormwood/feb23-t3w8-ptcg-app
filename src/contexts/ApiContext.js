38666b247d8ce90476ac701826d887a6204dd7c6
NickSpringall <123866019+NickSpringall@users.noreply.github.com>
2023-10-07 09:37:10 +1000

    init

--- src/contexts/ApiContext.js
+++ src/contexts/ApiContext.js
@@ -5,7 +5,7 @@ export const ApiContext = createContext(null);
 
 export default function ApiProvider ({children}){
 
-	const [apiUrl, setApiUrl] = useState("https://api.pokemontcg.io/");
+	const [apiUrl, setApiUrl] = useState("https://api.pokemontcg.io/v2/");
 
 	// useEffect(() => {
 	// 	setApiUrl("https://api.pokemontcg.io/");
